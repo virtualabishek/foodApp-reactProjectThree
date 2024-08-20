a3b9ab72599e3f20520114360b59d07d769e69ff
virtualabishek <717abishekneupane@gmail.com>
2024-08-20 06:34:38 +0545

    morning session started

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "a1727cc282ea4d3fa9ea280141bdcf93";
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("Pizza");
   //syntax of useEffect hook
   //useEffect(() => {}, [])
@@ -12,7 +12,8 @@ export default function Search() {
     async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
-      console.log(data.reults);
+      console.log(data.results);
+      setFoodData(data.results);
     }
     // function demo() {
     //   console.log("Demo function executed.");
