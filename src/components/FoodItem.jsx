4e70ac09e8ca488c282706bd88e2f35130a9ab8f
virtualabishek <717abishekneupane@gmail.com>
2024-08-21 14:40:16 +0545

    day session complete

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -2,9 +2,13 @@ import styles from "./footitem.module.css";
 export default function FoodItem({ food }) {
   return (
     <div className={styles.itemContainer}>
-      <img src={food.image} alt="" />
-      <h1>{food.title}</h1>
-      <button>View Reciepe</button>
+      <img className={styles.itemImage} src={food.image} alt="" />
+      <div className={styles.itemContent}>
+        <p className={styles.itemName}>{food.title}</p>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button className={styles.itemButton}>View Reciepe</button>
+      </div>
     </div>
   );
 }
